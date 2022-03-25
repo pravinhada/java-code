68fcbf1cdef4b273c451ab5f177908fa472b97a6
phada <phada@globallink.com>
2022-03-25 17:11:05 -0400

    facade

---
+++ design_patterns/structural/facade/FacadeClientDemo.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class FacadeClientDemo {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    }
+}
